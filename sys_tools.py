7c85b0a7f1b13373cdab3d3d1a548de3094fa773
zhhony <zhhony@126.com>
2022-06-20 13:53:52 +0800

    加上函数说明

--- sys_tools.py
+++ sys_tools.py
@@ -1,14 +1,15 @@
 from importlib import reload
 from types import ModuleType
 from typing import *
-import os
+from os import system
 
 
 def cls() -> None:
     """效果等同于windows终端的cls命令，可以清空当前终端所显示的内容（实质上是将终端内容移到上方直到超出屏幕显示范围）"""
-    os.system('cls')
+    system('cls')
     return None
 
 
 def rl(userModule: ModuleType) -> ModuleType:
+    """重载用户的包"""
     return reload(userModule)
