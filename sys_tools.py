a5b9a81e33fa9e61168c9e2fe823c414b5c3076f
zhhony <zhhony@126.com>
2022-06-20 10:42:25 +0800

    新增一个重载函数

--- sys_tools.py
+++ sys_tools.py
@@ -1,3 +1,6 @@
+from importlib import reload
+from types import ModuleType
+from typing import *
 import os
 
 
@@ -5,3 +8,7 @@ def cls() -> None:
     """效果等同于windows终端的cls命令，可以清空当前终端所显示的内容（实质上是将终端内容移到上方直到超出屏幕显示范围）"""
     os.system('cls')
     return None
+
+
+def rl(userModule: ModuleType) -> ModuleType:
+    return reload(userModule)
